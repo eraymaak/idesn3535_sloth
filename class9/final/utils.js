09768ca9ac4cad58aaff849a770a880675fa5672
eraymaak <31733372+eraymaak@users.noreply.github.com>
2017-12-03 18:54:07 -0500

    Final Site
    Made several alterations to final site. Made header viewable across all pages and styled header. Now need to add in shoes from API, make search bar functional, make site remember/display ratings, and make rating a possibility.

--- class9/final/utils.js
+++ class9/final/utils.js
@@ -1,30 +1,20 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<script>
-			function getParameterByName(name) {
-  				name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  				var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-      			results = regex.exec(location.search);
-  				return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-			};
-			function replaceString(string, key, value){
-  				const searchTerm = "{{" + key + "}}";
-  				return string.replace(searchTerm, value);
-			}
 
-			function processTemplate(template, context) {
- 			 Object.keys(context).forEach(function(key){
-   			 template = replaceString(template, key, context[key]);
-  			});
-  			return template;
-			}
-
-			function getTemplate(template, context) {
-  			return $(processTemplate(template, context));
-			}
-		</script>
-	</head>
-	<body>
-	</body>
-</html>
+function getParameterByName(name) {
+ 		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+ 		var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+   		results = regex.exec(location.search);
+ 		return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	};
+	function replaceString(string, key, value){
+ 		const searchTerm = "{{" + key + "}}";
+ 		return string.replace(searchTerm, value);
+	}
+function processTemplate(template, context) {
+ 	 Object.keys(context).forEach(function(key){
+ 			 template = replaceString(template, key, context[key]);
+ 		});
+ 		return template;
+	}
+function getTemplate(template, context) {
+ 		return $(processTemplate(template, context));
+	}
