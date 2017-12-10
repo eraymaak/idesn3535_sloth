d3f5e733061a7dc2ee6db5af2d95a02d44f823fa
eraymaak <31733372+eraymaak@users.noreply.github.com>
2017-12-10 16:46:32 -0500

    Final Project
    Working on developing final site functionality for specific shoe pages

---
+++ class13/rating-API.js
@@ -0,0 +1,29 @@
+const binID = "1et8db";
+
+function getRatings(){
+	let ratings;
+	$.ajax({
+		url: "https://api.myjson.com/bins/"+binID,
+		async: false,
+		success: function(data){
+			ratings = data;
+		}
+	});
+	return ratings;
+}
+
+function addRatings(rating){
+	const newRatings = this.getRatings();
+	newRatings.ratings.push(rating);
+	$.ajax({
+    	url:"https://api.myjson.com/bins/"+binID,
+   		type:"PUT",
+    	data:JSON.stringify(newRatings),
+    	contentType:"application/json; charset=utf-8",
+    	dataType:"json",
+    	async: false,
+   		success: function(data, textStatus, jqXHR){
+
+    	}
+	});    
+}
